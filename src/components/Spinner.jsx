a0cf456f8acc4c9e466a3a5e89dedc976f4b9828
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-06-10 14:45:02 +0600

    Refactor spinner to adjust size

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,6 +1,10 @@
-const Spinner = () => {
+const Spinner = ({ fullscreen = true }) => {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div
+      className={`${
+        fullscreen ? "h-screen" : "my-4"
+      } flex justify-center items-center`}
+    >
       <div className="relative m-auto">
         <div className="w-12 h-12 rounded-full absolute border-8 border-solid border-gray-200"></div>
         <div className="w-12 h-12 rounded-full animate-spin absolute border-8 border-solid border-accent_2 border-t-transparent"></div>
