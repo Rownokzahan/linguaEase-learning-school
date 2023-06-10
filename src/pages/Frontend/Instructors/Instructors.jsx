d665f8eee80f29d771d6528108f16119d4a862bd
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-06-10 16:41:22 +0600

    Show spinner when data is loading

--- src/pages/Frontend/Instructors/Instructors.jsx
+++ src/pages/Frontend/Instructors/Instructors.jsx
@@ -3,34 +3,49 @@ import { useEffect, useState } from "react";
 import Container from "../../../components/Container";
 import SectionTitle from "../../../components/SectionTitle";
 import InstructorCard from "../../../components/cards/InstructorCard";
+import Spinner from "../../../components/Spinner";
 
 const Instructors = () => {
-      const [instructors, setInstructors] = useState([]);
-      useEffect(() => {
-        axios
-          .get(`${import.meta.env.VITE_API_URL}/instructors`)
-          .then((res) => {
-            setInstructors(res?.data);
-          });
-      }, []);
-    
-    return (
-      <Container>
-        <SectionTitle label={"All Instructors"} />
+  const [isLoading, setIsLoading] = useState(true);
+  const [instructors, setInstructors] = useState([]);
 
-        {instructors &&
-        Array.isArray(instructors) &&
-        instructors?.length > 0 ? (
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-            {instructors?.map((instructor) => (
-              <InstructorCard key={instructor._id} instructor={instructor} />
-            ))}
-          </div>
-        ) : (
-          "No Instructor found"
-        )}
-      </Container>
-    );
+  useEffect(() => {
+    const fetchInstructors = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/instructors`
+        );
+        setInstructors(response?.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("Error fetching instructors:", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchInstructors();
+  }, []);
+
+  const hasInstructors =
+    instructors && Array.isArray(instructors) && instructors.length > 0;
+
+  return (
+    <Container>
+      <SectionTitle label={"All Instructors"} />
+
+      {isLoading ? (
+        <Spinner fullscreen={false} />
+      ) : hasInstructors ? (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {instructors.map((instructor) => (
+            <InstructorCard key={instructor._id} instructor={instructor} />
+          ))}
+        </div>
+      ) : (
+        "No instructors found"
+      )}
+    </Container>
+  );
 };
 
 export default Instructors;
--- src/pages/Frontend/Programs/Programs.jsx
+++ src/pages/Frontend/Programs/Programs.jsx
@@ -1,26 +1,43 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Container from '../../../components/Container';
-import SectionTitle from '../../../components/SectionTitle';
-import ProgramCard from '../../../components/cards/ProgramCard';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Container from "../../../components/Container";
+import SectionTitle from "../../../components/SectionTitle";
+import ProgramCard from "../../../components/cards/ProgramCard";
+import Spinner from "../../../components/Spinner";
 
 const Programs = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [programs, setPrograms] = useState([]);
+
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_API_URL}/programs`)
-      .then((res) => {
-        setPrograms(res?.data);
-      });
+    const fetchPrograms = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/programs`
+        );
+        setPrograms(response?.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("Error fetching programs:", error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchPrograms();
   }, []);
+
+  const hasPrograms =
+    programs && Array.isArray(programs) && programs.length > 0;
 
   return (
     <Container>
       <SectionTitle label={"All Programs"} />
 
-      {programs && Array.isArray(programs) && programs?.length > 0 ? (
+      {isLoading ? (
+        <Spinner fullscreen={false} />
+      ) : hasPrograms ? (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {programs?.map((program) => (
+          {programs.map((program) => (
             <ProgramCard key={program._id} program={program} />
           ))}
         </div>
