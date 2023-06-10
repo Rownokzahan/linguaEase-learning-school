6ba5b367fa49a0b24648b519fff8cb2a2d447b08
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-06-10 14:56:38 +0600

    Add show select button based on user funtionality

--- src/components/cards/ProgramCard.jsx
+++ src/components/cards/ProgramCard.jsx
@@ -1,8 +1,13 @@
 import { IoCalendar } from "react-icons/io5";
 import { SiGoogleclassroom } from "react-icons/si";
 import { AiOutlinePlus } from "react-icons/ai";
+import { addToSelecedPrograms } from "../../api/selectedPrograms";
+import { useLocation, useNavigate } from "react-router";
+import { toast } from "react-hot-toast";
 
-const ProgramCard = ({ program }) => {
+const ProgramCard = ({ program, userRole }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
   const {
     title,
     displayImage,
@@ -14,9 +19,24 @@ const ProgramCard = ({ program }) => {
   } = program || {};
 
   const availableSeats = capacity - enrolled;
+  const hideButton =
+    !availableSeats || userRole === "admin" || userRole === "instructor";
+
+  const handleSelectedProgram = () => {
+    if (userRole === "none") {
+      toast.error("You have to login first to select program");
+      navigate("/login", { state: { from: location }, replace: true });
+      return;
+    }
+    addToSelecedPrograms(program);
+  };
 
   return (
-    <div className="border rounded-2xl shadow group relative ">
+    <div
+      className={`border rounded-2xl shadow group relative ${
+        !availableSeats && "bg-red-400"
+      }`}
+    >
       <div className="p-4">
         <img
           src={displayImage}
@@ -28,7 +48,7 @@ const ProgramCard = ({ program }) => {
       <div className="p-6">
         <div className="flex justify-between items-center">
           <div>
-            <p className="text-gray-400">{instructor}&apos;s</p>
+            <p className="text-gray-500">{instructor}&apos;s</p>
             <h4 className="text-xl font-semibold">{title}</h4>
           </div>
           <p className="text-2xl text-accent_2">${price}</p>
@@ -43,11 +63,18 @@ const ProgramCard = ({ program }) => {
           </p>
         </div>
       </div>
-      <div className="cursor-pointer group-hover:flex duration-150 bg-gray-300 bg-opacity-30 hidden absolute inset-0 justify-center items-center rounded-2xl">
-        <button className=" bg-accent_2 text-white font-semibold p-3 rounded-full ">
-          <AiOutlinePlus className="text-4xl mx-auto" />
+
+      {/* Add to selected programs button */}
+      {!hideButton && (
+        <button
+          onClick={handleSelectedProgram}
+          className="cursor-pointer group-hover:flex duration-150 bg-gray-300 bg-opacity-30 hidden absolute inset-0 justify-center items-center rounded-2xl"
+        >
+          <span className="bg-accent_2 text-white font-semibold p-3 rounded-full ">
+            <AiOutlinePlus className="text-4xl mx-auto" />
+          </span>
         </button>
-      </div>
+      )}
     </div>
   );
 };
