ec950695d961147eeb758ee9768c0725643b4b0e
Rownokzahan <84782117+Rownokzahan@users.noreply.github.com>
2023-06-10 16:12:48 +0600

    Upadate PopularPrograms

--- src/pages/Frontend/Home/PopularPrograms.jsx
+++ src/pages/Frontend/Home/PopularPrograms.jsx
@@ -3,29 +3,52 @@ import axios from "axios";
 import ProgramCard from "../../../components/cards/ProgramCard";
 import Container from "../../../components/Container";
 import SectionTitle from "../../../components/SectionTitle";
+import useUserRole from "../../../hooks/useUserRole";
+import Spinner from "../../../components/Spinner";
 
 const PopularPrograms = () => {
+  const [isProgramsLoading, setIsProgramsLoading] = useState(true);
   const [programs, setPrograms] = useState([]);
+  const [userRole, isUserRoleLoading] = useUserRole();
+
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_API_URL}/programs/popular`)
-      .then((res) => {
-        setPrograms(res?.data);
-      });
+    const fetchPopularPrograms = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/programs/popular`
+        );
+        setPrograms(response.data);
+        setIsProgramsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsProgramsLoading(false);
+      }
+    };
+
+    fetchPopularPrograms();
   }, []);
-  
+
+  const isLoading = isUserRoleLoading || isProgramsLoading;
+  const hasPrograms =
+    programs && Array.isArray(programs) && programs.length > 0;
+
   return (
     <Container>
-      <SectionTitle label={"Popular Programs"} />
+      <SectionTitle label="Popular Programs" />
 
-      {programs && Array.isArray(programs) && programs?.length > 0 ? (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {programs?.map((program) => (
-            <ProgramCard key={program._id} program={program} />
-          ))}
-        </div>
+      {isLoading ? (
+        <Spinner fullscreen={false} />
       ) : (
-        "No programs found"
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {hasPrograms &&
+            programs.map((program) => (
+              <ProgramCard
+                key={program._id}
+                program={program}
+                userRole={userRole}
+              />
+            ))}
+        </div>
       )}
     </Container>
   );
